b0f14e2fb3dcb979cc8adf633f0456f3a7c03203
Victor Machado <victormachadogp@gmail.com>
2024-08-24 18:26:15 -0300

    Fetching users

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
-<script setup>
+<template>
+  <button @click="fetchUsers">Fetch Users</button>
+</template>
+  
+<script>
+import { ref, computed } from 'vue'
 
+export default {
+  setup() {
+    const users = ref([])
+
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        if (!response.ok) throw new Error('Failed to fetch users')
+        users.value = await response.json()
+        console.log(users.value)
+      } catch (err) {
+        error.value = err.message
+      }
+    }
+
+    return {
+      fetchUsers
+    }
+  }
+}
 </script>
 
-<template>
-
-  <main>
-    Some content
-  </main>
-  
-</template>
-
 <style scoped>
-
 </style>
