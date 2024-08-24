6bbba986ebc703f418d5264bfce6fbed55e00603
Victor Machado <victormachadogp@gmail.com>
2024-08-24 18:43:56 -0300

    Basic table w/ users, loading and error displayed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
   <button @click="fetchUsers">Fetch Users</button>
+
+  <p v-if="error" style="color: red">{{ error }}</p>
+
+  <table v-if="users.length">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>City</th>
+        <th>Company</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.name }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.phone }}</td>
+        <td>{{ user.address.city }}</td>
+        <td>{{ user.company.name }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
   
 <script>
@@ -8,20 +33,26 @@ import { ref, computed } from 'vue'
 export default {
   setup() {
     const users = ref([])
+    const loading = ref(false)
+    const error = ref('')
 
     const fetchUsers = async () => {
+      loading.value = true
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const response = await fetch('https://jsonplaceholder.typicode.com/users1')
         if (!response.ok) throw new Error('Failed to fetch users')
         users.value = await response.json()
-        console.log(users.value)
       } catch (err) {
         error.value = err.message
+      } finally {
+        loading.value = false
       }
     }
 
     return {
-      fetchUsers
+      users,
+      fetchUsers,
+      error
     }
   }
 }
