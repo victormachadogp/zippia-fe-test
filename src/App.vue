e069f8cdcd89debe2fc5ead56943071d0e6e31cc
Victor Machado <victormachadogp@gmail.com>
2024-08-24 19:06:55 -0300

    Search users and filter input implemented

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
+  <input v-model="searchUsers" placeholder="Search by name..." @input="filterUsers" />
   <button @click="fetchUsers">Fetch Users</button>
 
   <p v-if="error" style="color: red">{{ error }}</p>
 
-  <table v-if="users.length">
+  <table v-if="hasFilteredUsers">
     <thead>
       <tr>
         <th>Name</th>
@@ -15,7 +16,7 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="user in users" :key="user.id">
+      <tr v-for="user in filteredUsers" :key="user.id">
         <td>{{ user.name }}</td>
         <td>{{ user.username }}</td>
         <td>{{ user.email }}</td>
@@ -25,6 +26,9 @@
       </tr>
     </tbody>
   </table>
+
+  <p v-if="!hasFilteredUsers && !loading">No users found.</p>
+  <p v-if="loading">Loading...</p>
 </template>
   
 <script>
@@ -33,15 +37,17 @@ import { ref, computed } from 'vue'
 export default {
   setup() {
     const users = ref([])
+    const searchUsers = ref('')
     const loading = ref(false)
-    const error = ref('')
+    const error = ref(null)
 
     const fetchUsers = async () => {
       loading.value = true
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users1')
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
         if (!response.ok) throw new Error('Failed to fetch users')
         users.value = await response.json()
+        console.log(users.value)
       } catch (err) {
         error.value = err.message
       } finally {
@@ -49,9 +55,22 @@ export default {
       }
     }
 
+    const filterUsers = () => {
+      return users.value.filter((user) =>
+        user.name.toLowerCase().includes(searchUsers.value.toLowerCase())
+      )
+    }
+
+    const filteredUsers = computed(() => filterUsers())
+
+    const hasFilteredUsers = computed(() => filteredUsers.value.length > 0)
+
     return {
-      users,
+      searchUsers,
       fetchUsers,
+      filteredUsers,
+      hasFilteredUsers,
+      loading,
       error
     }
   }
